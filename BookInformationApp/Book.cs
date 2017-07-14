150978d150cd06096b87dd4a5efd1bd7dfb7e6a1
PardeepS2 <Pardeemaloya@gmail.com>
2017-07-13 20:12:17 -0400

    created classes Book and Author

---
+++ BookInformationApp/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInformationApp
+{
+    class Book
+    {
+        private string name;
+       // private  Author[];
+        private double price;
+        private DateTime publication;
+        private int qty = 0;
+
+        public Book(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public Book(string name, double price, int qty)
+        {
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+        }
+
+
+    }
+}
